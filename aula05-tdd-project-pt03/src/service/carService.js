0bdc8078b672a4f36390db87d4af7fdc7a89a147
Matheus <matheuscavagnari77@gmail.com>
2023-02-14 21:39:40 -0300

    feat: criação de todos os teste

--- aula05-tdd-project-pt03/src/service/carService.js
+++ aula05-tdd-project-pt03/src/service/carService.js
@@ -1,5 +1,6 @@
 const BaseRepository = require('./../repository/base/baseRepository')
 const Tax = require('./../entities/tax')
+const Transaction = require('./../entities/transaction')
 class CarService {
     constructor({ cars }) {
         this.carRepository = new BaseRepository({ file: cars})
@@ -42,6 +43,26 @@ class CarService {
        const formattedPrice = this.currencyFormat.format(finalPrice)
 
        return formattedPrice
+    }
+
+    async rent(
+        customer, carCategory, numberOfDays
+    ){
+        const car = await this.getAvailableCar(carCategory)
+        const finalPrice = await this.calculateFinalPrice(customer, carCategory, numberOfDays)
+
+        const today = new Date()
+        today.setDate(today.getDate() + numberOfDays)
+        const options = { year: "numeric", month: "long", day: "numeric"}
+        const dueDate= today.toLocaleDateString("pt-br", options)
+
+        const transaction = new Transaction({
+            customer,
+            dueDate,
+            car,
+            amount: finalPrice
+        })
+        return transaction
     }
 }
 
