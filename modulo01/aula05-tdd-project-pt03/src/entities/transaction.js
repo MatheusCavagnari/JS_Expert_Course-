823e31a2c9e1c455f05299f8ccf0165bc5d3ac66
Matheus <matheuscavagnari77@gmail.com>
2023-02-22 19:14:05 -0300

    auteração de estrutura de pastas

---
+++ modulo01/aula05-tdd-project-pt03/src/entities/transaction.js
@@ -0,0 +1,11 @@
+class Transaction {
+    constructor({custmer, car, amount, dueDate}){
+        this.custmer = custmer 
+        this.car = car 
+        this.amount = amount 
+        this.dueDate = dueDate 
+    }
+
+}
+
+module.exports = Transaction
