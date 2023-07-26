daa19e350e3f42f1b6547cb5608b45dde7242ee0
Matheus <matheuscavagnari77@gmail.com>
2023-07-26 17:44:24 -0300

    estrutura do factory

---
+++ modulo06/creational/02.abstract-factory/shared/base/viewFactory.mjs
@@ -0,0 +1,8 @@
+import NotImplementedException from "../notImplementedException.mjs";
+
+
+export default class ViewFactory {
+  createTable() {
+    throw new NotImplementedException(this.createTable.name)
+  }
+}
