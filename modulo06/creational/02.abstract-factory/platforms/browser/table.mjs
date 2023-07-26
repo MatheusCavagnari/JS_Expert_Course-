5fa0bf7f5bcb6a14ae48b05164a69287d7d6e217
Matheus <matheuscavagnari77@gmail.com>
2023-07-26 18:38:20 -0300

    feat: Padrão de projeto `Abstract Factory`

--- modulo06/creational/02.abstract-factory/platforms/browser/table.mjs
+++ modulo06/creational/02.abstract-factory/platforms/browser/table.mjs
@@ -2,6 +2,29 @@ import TableComponent from "../../shared/base/tableComponent.mjs";
 
 export default class TableBrowserComponent extends TableComponent {
   render(data) {
-    window.alert('chamou!!')
+    const template = this.prepareData(data)
+    document.body.insertAdjacentHTML("afterbegin", template)
+  }
+
+  prepareData(data) {
+    const [firstItem] = data
+    const tHeaders = Object.keys(firstItem).map(text => `<th scope=col>${text}</th>`)
+    const joinList = list => list.join('')
+    const tBoryValues = data
+      .map(item => Object.values(item))
+      .map(item => item.map(values => `<td>${values}</td>`))
+      .map(tds => `<tr>${joinList(tds)}</tr>`)
+
+    const template = `
+    <table class="table">
+      <thead>
+        <tr>${joinList(tHeaders)}</tr>
+      </thead>
+      <tbody>
+        ${joinList(tBoryValues)}
+      </tbody>
+    </table>`
+
+    return template
   }
 }
